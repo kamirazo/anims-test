fe141abe8957550d31f810a4ac328e63b6d3efd7
Camille GICQUEL <camille.gicquel@razorfish.fr>
2024-05-31 18:33:58 +0200

    feat(gallerySequence): manage fluid timeline

--- src/app/components/gallerySequence/gallerySequence.tsx
+++ src/app/components/gallerySequence/gallerySequence.tsx
@@ -4,6 +4,88 @@ import useScrollImageSequenceFramerCanvas from "@/app/hooks/useScrollImageSequen
 import { CSSProperties, useMemo, useRef } from "react";
 
 import './gallerySequence.scss'
+import SectionTitle from "../sectionTitle/sectionTitle";
+import AnimatedList from "../animatedList/AnimatedList";
+import { motion, useTransform } from "framer-motion";
+
+type SubComponentSettings = {
+  animType?: 'glowing' | 'translateY'
+  posX?: string
+  posY?: string
+  start?: number
+  duration?: number
+  fixed: boolean
+}
+
+type AnimatedListSubComponent = {
+  componentID: 'AnimatedList'
+  listItems: string[]
+}
+
+type SectionTitleSubComponent = {
+  componentID: 'SectionTitle'
+  titleContent: string
+  subtitle?: string
+  titleTag?: keyof HTMLElementTagNameMap
+}
+
+type VideoOnScrollContent = {
+  content: string | AnimatedListSubComponent | SectionTitleSubComponent
+  settings?: SubComponentSettings
+}
+
+const timelineClass = (isFixed: boolean, isItem: boolean) => {
+  let classname = 'timeline'
+
+  if (isFixed) {
+    classname = `fixed-${classname}`
+  }
+
+  if (isItem) {
+    classname = `${classname}__item`
+  }
+
+  return classname
+}
+
+const timelineItemStatus = (
+  currentScroll: number,
+  scrollStart?: number,
+  scrollDuration?: number) => {
+  if (scrollStart === undefined || scrollDuration === undefined || currentScroll < scrollStart) return ''
+
+  if (currentScroll > scrollStart + scrollDuration) return 'passed'
+
+  if (currentScroll > scrollStart) return 'displayed'
+}
+
+const returnContentComponent = (componentContent: VideoOnScrollContent) => {
+  if (typeof componentContent.content === 'string') {
+    return (
+      <div>{componentContent.content}</div>
+    )
+  }
+
+  switch (componentContent.content.componentID) {
+    case 'SectionTitle':
+      return (
+        <SectionTitle 
+          titleTag={componentContent.content.titleTag} 
+          titleContent={componentContent.content.titleContent ?? ''} 
+          animType={componentContent.settings?.animType} 
+          subtitleContent={componentContent.content.subtitle ?? ''}  />
+      )
+    case 'AnimatedList':
+      return (
+        <AnimatedList 
+          listItems={componentContent.content.listItems ?? []} />
+      )
+    default:
+      return (
+        <div>{componentContent.content}</div>
+      )
+  }
+}
 
 const createImage = (src: string) => {
   if (typeof window === 'undefined') return
@@ -41,7 +123,9 @@ const handleDrawCanvas = (
 
 const imgSequenceLength = 215
 
-const GallerySequence = () => {
+const GallerySequence = ({content}: {
+  content?: VideoOnScrollContent[]
+}) => {
   const keyframes = useMemo(
     () =>
       [...new Array(imgSequenceLength)].map((_, i) =>
@@ -64,6 +148,44 @@ const GallerySequence = () => {
     },
   });
 
+  const maskStarts = content?.map(contentItem => (contentItem?.settings?.start ?? 0))
+  const maskPos = content?.map(contentItem => {
+    switch (contentItem?.settings?.posY) {
+      case 'start':
+        return '0'
+      case 'center':
+        return '0'
+      case 'end':
+        return '50vh'
+      default:
+        return '';
+    }
+  })
+  const maskHeights = content?.map(contentItem => {
+    switch (contentItem?.settings?.posY) {
+      case 'start':
+        return '50vh'
+      case 'center':
+        return '100vh'
+      case 'end':
+        return '50vh'
+      default:
+        return '';
+    }
+  })
+
+  const maskTop = useTransform(
+    progress,
+    maskStarts ?? [0, 1],
+    maskPos ?? ['', '']
+  )
+  
+  const maskComputedHeights = useTransform(
+    progress,
+    maskStarts ?? [0, 1],
+    maskHeights ?? ['', '']
+  )
+
   return (
     <section 
       ref={containerRef}
@@ -72,6 +194,34 @@ const GallerySequence = () => {
       <div className="gallery-sequence-wrapper">
         <canvas ref={canvasRef} className="gallery-sequence__canvas" />
       </div>
+
+      {content && (
+        <div className="gallery-sequence__content">
+          <motion.div className="timeline__mask" style={{
+            '--scrollValue': progress,
+            '--maskTop': maskTop,
+            '--maskHeight': maskComputedHeights
+            } as CSSProperties}>
+            <div className={timelineClass(false, false)}>
+              {content.map((currentContent, index) => (
+                <motion.div key={index}
+                  className={`timeline__item ${currentContent.settings?.posX}-x ${currentContent.settings?.posY}-y`}
+                  // className={timelineItemStatus(
+                  //   progress,
+                  //   currentContent.settings?.start,
+                  //   currentContent.settings?.duration)}
+                  style={{
+                    '--start': currentContent.settings?.start,
+                    '--duration': currentContent.settings?.duration
+                  } as CSSProperties}>
+                  
+                  { returnContentComponent(currentContent) }
+                </motion.div>
+              ))}
+            </div>
+          </motion.div>
+        </div>
+      )}
     </section>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,50 @@ export default function ScrollVideoOnly() {
     <main>
       {/* TODO : Uncomment after adding a test video under /public/video/test2.mp4 */}
       {/* <VideoOnScrollSection videoURL="/video/test2.mp4" playbackSpeed={200}/> */}
-      <GallerySequence />
+      <GallerySequence 
+        content={[
+          {
+            content: {
+              componentID: 'SectionTitle',
+              titleContent: 'Animated Image Sequence'
+            },
+            settings: {
+              start: 0,
+              duration: .1,
+              posX: 'center',
+              posY: 'center',
+              fixed: false
+            }
+          },
+          {
+            content: {
+              componentID: 'AnimatedList',
+              listItems: [
+                'Side text 1. Nobis minus aut sint eum.',
+                'Side text 2. Nobis minus aut sint eum.',
+                'Side text 3. Nobis minus aut sint eum.'
+              ]
+            },
+            settings: {
+              start: .2,
+              duration: .2,
+              posX: 'end',
+              posY: 'center',
+              fixed: false
+            }
+          },
+          {
+            content: 'Centered end text. Nobis minus aut sint eumst',
+            settings: {
+              start: .7,
+              duration: .2,
+              posX: 'center',
+              posY: 'end',
+              fixed: false
+            }
+          }
+        ]}
+      />
     </main>
   )
 }
